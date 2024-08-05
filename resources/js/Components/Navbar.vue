630ee1a36909159f61ae00ee8738af12afd8cc2f
mbolodecasaneo <mbolo@casaneo.io>
2024-08-05 18:08:00 +0200

    update

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -197,11 +197,7 @@
             <div class="ml-4 flex lg:ml-0">
               <a href="#">
                 <span class="sr-only">Your Company</span>
-                <img
-                  class="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
+                <img class="h-20 w-20" src="../assets/juc.png" alt="" />
               </a>
             </div>
 
@@ -332,18 +328,6 @@
                 <a href="#" class="text-sm font-medium text-gray-700 hover:text-gray-800"
                   >Create account</a
                 >
-              </div>
-
-              <div class="hidden lg:ml-8 lg:flex">
-                <a href="#" class="flex items-center text-gray-700 hover:text-gray-800">
-                  <img
-                    src="https://tailwindui.com/img/flags/flag-canada.svg"
-                    alt=""
-                    class="block h-auto w-5 flex-shrink-0"
-                  />
-                  <span class="ml-3 block text-sm font-medium">CAD</span>
-                  <span class="sr-only">, change currency</span>
-                </a>
               </div>
 
               <!-- Search -->
